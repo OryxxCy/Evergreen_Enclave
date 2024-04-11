bd6ce175c7530046bdfa939318d8cf15545fdae7
Cyrus <Cyrus.bi3n@gmail.com>
2024-04-11 02:16:30 -0500

    Added session cart

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,32 @@
+class CartController < ApplicationController
+  def create
+    product_id = params[:id]
+    quantity = params[:quantity].to_i
+    if session[:shopping_cart].key?(product_id)
+      session[:shopping_cart][product_id] += quantity
+    else
+      session[:shopping_cart][product_id] = quantity
+    end
+
+    plant = Plant.find(product_id)
+    flash[:notice] = "#{quantity} #{plant.name} added to cart."
+  end
+
+  def destroy
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    plant = Plant.find(id)
+
+    flash[:notice] = "#{plant.name} removed from cart."
+  end
+
+  def index
+    cart_items = session[:shopping_cart]
+    @cart_products = Plant.where(id: cart_items.keys)
+    @cart_items_with_quantity = {}
+    @cart_products.each do |product|
+      quantity = cart_items[product.id.to_s]
+      @cart_items_with_quantity[product] = quantity
+    end
+  end
+end
